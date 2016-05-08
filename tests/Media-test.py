89b5988d54d1928cc73a1106c4cfd4e13b57f35c
lethargilistic <mikeoverby@outlook.com>
2016-05-07 23:32:36 -0700

    More Media tests, get_state and odds and ends

--- tests/Media-test.py
+++ tests/Media-test.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 
 try:
     import vlc
@@ -7,9 +8,9 @@ except ImportError:
 
 class TestMedia(unittest.TestCase):
     def setUp(self):
-        self.real_m = vlc.Media("file://./assets/ChopinOp9No2.mp3")
+        self.real_m = vlc.Media("file://assets/ChopinOp9No2.mp3")
 
-        self.mock_m = vlc.Media("file://./assets/ChopinOp9No2.mp3")
+        self.mock_m = vlc.Media("file://assets/ChopinOp9No2.mp3")
         self.mock_m.set_meta(vlc.Meta.Title, 'Title')
         self.mock_m.set_meta(vlc.Meta.Artist, 'Artist')
         self.mock_m.set_meta(vlc.Meta.Genre, 'Genre')
@@ -114,4 +115,76 @@ class TestMedia(unittest.TestCase):
 
     #def test_get_meta_disctotal(self):
     #    self.assertEqual(self.mock_m.get_meta(vlc.Meta.DiscTotal), 'DiscTotal')
+
+    def test_get_mrl(self):
+        self.assertEqual(self.real_m.get_mrl(), "file://assets/ChopinOp9No2.mp3")
+
+    def test_get_state_nothing_special(self):
+        self.assertEqual(self.real_m.get_state(), vlc.State.NothingSpecial)
+
+    def test_get_state_opening(self):
+        player = vlc.MediaPlayer()
+        player.set_media(self.real_m)
+        player.play()
+        self.assertEquals(self.real_m.get_state(), vlc.State.Opening)
+
+    def test_get_state_playing(self):
+        player = vlc.MediaPlayer()
+        player.set_media(self.real_m)
+        player.play()
+        while self.real_m.get_state() == vlc.State.Opening:
+            pass
+        self.assertEquals(self.real_m.get_state(), vlc.State.Playing)
+
+    def test_get_state_paused(self):
+        player = vlc.MediaPlayer()
+        player.set_media(self.real_m)
+        player.play()
+        while self.real_m.get_state() == vlc.State.Opening:
+            pass
+        player.pause()
+        self.assertEquals(self.real_m.get_state(), vlc.State.Paused)
+
+    def test_get_state_paused_then_resumed(self):
+        player = vlc.MediaPlayer()
+        player.set_media(self.real_m)
+        player.play()
+        while self.real_m.get_state() == vlc.State.Opening:
+            pass
+        player.pause()
+        player.pause()
+        self.assertEquals(self.real_m.get_state(), vlc.State.Playing)
+
+    def test_get_state_stopped(self):
+        player = vlc.MediaPlayer()
+        player.set_media(self.real_m)
+        player.play()
+        while self.real_m.get_state() == vlc.State.Opening:
+            pass
+        player.stop()
+        self.assertEquals(self.real_m.get_state(), vlc.State.Stopped)
+
+    #TODO, not sure how to advance it to the end.
+    #def test_get_state_ended(self):
+    #    player = vlc.MediaPlayer()
+    #    player.set_media(self.real_m)
+    #    player.play()
+    #    
+    #    self.assertEquals(self.real_m.get_state(), vlc.State.Stopped)
+
+    def test_is_parsed_false(self):
+        self.assertFalse(self.real_m.is_parsed())
+
+    def test_is_parsed_true(self):
+        self.real_m.parse()
+        self.assertTrue(self.real_m.is_parsed())
+
+    def test_duplicate(self):
+        dup = self.real_m.duplicate()
+        self.assertEqual(self.real_m.get_mrl(), dup.get_mrl())
+
+    def test_player_new_from_media(self):
+        otherplayer = vlc.MediaPlayer("file://assets/ChopinOp9No2.mp3")
+        self.assertEqual(self.real_m.player_new_from_media().get_media().get_mrl(), 
+                         otherplayer.get_media().get_mrl())
 
